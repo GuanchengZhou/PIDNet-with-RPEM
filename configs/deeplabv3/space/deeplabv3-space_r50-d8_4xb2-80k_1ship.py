7a5a832b60711eabb3c37d74ad12f3de3d9876f1
DorgCN <zhouguancheng2002@163.com>
2024-02-24 11:04:53 +0800

    submit

--- configs/deeplabv3/space/deeplabv3-space_r50-d8_4xb2-80k_1ship.py
+++
@@ -1,7 +0,0 @@
-_base_ = [
-    '../../_base_/models/deeplabv3-space_r50-d8.py', '../../_base_/datasets/my_city_1ship_real.py',
-    '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_80k.py'
-]
-crop_size = (536, 960)
-data_preprocessor = dict(size=crop_size)
-model = dict(data_preprocessor=data_preprocessor)
